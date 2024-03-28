bfbfb334b7c6d311b299512a21e3b2ff4212af7b
Juan Carlos Torres Gutiérrez <118766109+Jcee02@users.noreply.github.com>
2024-03-27 19:56:42 -0600

    Command line utility flags for filepath and new entry WIP

--- src/main.c
+++ src/main.c
@@ -1,26 +1,51 @@
 #include <stdio.h>
+#include <getopt.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include "../include/file.h"
+#include "../include/parse.h"
 
-#include "file.h"
-#include "parse.h"
 
+void print_usage(char**);
 
-int main(int argc, char *argv[1]){
-  int fd, numItems = 0;
+int main(int argc, char *argv[]){
+    
+    char *filepath = NULL;
+    bool newfile = false;
+    int c;
 
-  if (argc != 2) {
-    printf("Usage: %s <filename>", argv[0]);
+    while ((c = getopt(argc, argv, "nf:")) != -1) {
+        switch(c){
+            case 'n':
+                newfile = true;
+                break;
+            case 'f':
+                filepath = optarg;
+                break;
+            case '?':
+                printf("Unknown option -%c\n", c);
+                break;
+            default:
+                return -1;
+        }
+    }
+
+    if (filepath == NULL) {
+        printf("File path is a required argument.\n");
+        print_usage(argv);
+        return EXIT_SUCCESS;
+    }
+
+    printf("Newfile: %d\n", newfile);
+    printf("File path: %s\n", filepath); 
+    
     return EXIT_SUCCESS;
-  }
-  
-  fd = open_file_rw(argv[1]);
-   if (fd == -1) 
-    return -1;
+}
 
-  if (parse_database_header(fd, &numItems))
-    return -1;
 
-  printf("Number of items stored: %u\n", numItems);
-
-  return EXIT_SUCCESS;
+void print_usage(char *argv[]) {
+      printf("Usage: %s -n -f <database file>\n", argv[0]);
+      printf("\t -n   - create new database file\n");
+      printf("\t -f   - (required) path to database file\n");
+      return;
 }
