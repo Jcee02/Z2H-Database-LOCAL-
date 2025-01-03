66ebf1a4158b565269278b04291ec33fdfe2a990
Juan Carlos Torres <ccdvc2002@gmail.com>
2025-01-03 16:59:07 -0600

    Added rd_employee and add_employee functions

--- include/parse.h
+++ include/parse.h
@@ -7,6 +7,22 @@ struct db_header_t {
 };
 
 
+<<<<<<< HEAD
 int parse_database_header(int, int*);
+=======
+
+struct employee_t {
+  char name[256];
+  char addr[256];
+  unsigned int hours;
+};
+
+int create_db_header(int fd, struct db_header_t **headerOut);
+int validate_db_header(int fd, struct db_header_t **headerOut);
+// employee_t** is a heap-allocated employee buffer we are writing to -> main
+int rd_employees(int fd, struct db_header_t *, struct employee_t **);
+int add_employee(struct db_header_t*, struct employee_t*, char* addstring);
+void output_file(int fd, struct db_header_t *);
+>>>>>>> e233001 (Added rd_employee and add_employee functions)
 
 #endif
